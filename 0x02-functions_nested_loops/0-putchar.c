5bb84213265446fe81533915877e2a402597c179
hizkiastesfaye <tesfayehizkias@gmail.com>
2022-12-13 03:53:45 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,16 @@
-#!/bin/bash
+#include "main.h"
 
+int main(void)
+{
+	char a[] = "_putchar";
+	int b;
+	int c = sizeof(a) / sizeof(a[0]);
+
+	for (b = 0; b < c; b++)
+	{
+		_putchar (a[b]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
