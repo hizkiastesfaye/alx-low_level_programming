256f7ba3f1710e6ae2bc7452374cbe277a6013bb
hizkiastesfaye <tesfayehizkias@gmail.com>
2022-12-14 05:31:04 -0800

    lower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ *_isalpha - prints all alphabets
+ * @c: parameter
+ * Description: to print alphabets
+ * Return: alphabets
+ */
 int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
