265ed29ce8720dd5245d002baf80fa0cfacf9884
hizkiastesfaye <tesfayehizkias@gmail.com>
2022-12-14 04:33:51 -0800

    sign

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ *:
+ */
+
+void print_to_98(int n)
+{
+	int b;
+	if (a >= 98)
+	{
+		for (b = n; b >= 98; b--)
+		{
+			printf("%d, ",b);
+		}
+	}
+	else
+	{
+		for (b = n; b <= 98; b++)
+		{
+			printf("%d, ",b);
+		}
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * print_sign - to print signs
+ * @n: param1
+ * Description: no way to do it
+ * Return: sign
+ */
 int print_sign(int n)
 {
 	if (n > 0)
