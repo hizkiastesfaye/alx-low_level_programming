67128815d87a1843faf762d0358d5f5db53bb2ac
hizkiastesfaye <tesfayehizkias@gmail.com>
2022-12-14 05:42:11 -0800

    lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - to print a-z
+ */
 void print_alphabet_x10(void)
 {
 	char a;
 	int b;
 
-	for (b = 0; b < 11; b++)
+	for (b = 0; b < 10; b++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 		{
