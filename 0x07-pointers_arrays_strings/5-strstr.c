01502cc02afc5917ef3cec9749ac917c20760f5f
hizkiastesfaye <tesfayehizkias@gmail.com>
2023-01-16 03:49:49 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr -function to search string
+ * @haystack: pointer param of string
+ * @needle: pointer param of sustring
+ * Return: return the string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (*(needle + i + 1) == '\0')
+					return (haystack);
+				i++;
+			} while (*(haystack + i) == *(needle + i));
+		}
+		haystack++;
+	}
+	return ('\0');
+}
