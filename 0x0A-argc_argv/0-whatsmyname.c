68c9e4cbcd7f892d5823d8d0699531e7a8ed063e
hizkiastesfaye <tesfayehizkias@gmail.com>
2023-01-21 01:05:51 -0800

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: count argu
+ * @argv: array of pointer argument
+ * Return: return 0;
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
