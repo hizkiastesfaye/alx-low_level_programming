c52bc1f16096cc8794a5830a002083b40e96cef5
hizkiastesfaye <tesfayehizkias@gmail.com>
2023-01-25 01:21:36 -0800

    malloc calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,16 +3,19 @@
  * _calloc - function calloc
  * @nmemb: number of byte
  * @size: size of byte
- * return: return value
+ * Return: return value
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char **pp;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	pp = malloc(size * nmemb);
 	if (pp == NULL)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		pp[i] = 0;
 	return (pp);
 }
